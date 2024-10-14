e2973a6e3f17569fa8fcf5b2e90cff81de2fde62
Giulia D'Angelo <giulia.dangelo.1990@gmail.com>
2024-10-15 00:00:35 +0200

    adjust tutorial

--- tutorialretina.py
+++ tutorialretina.py
@@ -29,6 +29,7 @@ matplotlib.use('qt5agg')  # Sets up Matplotlib to use the Qt5 backend for graphi
 # Generate a Poisson-distributed tensor representing a spike train over 'lenstim' time steps
 rate_spikes = 5
 ts = torch.poisson(torch.ones(1, lenstim, 1) * rate_spikes).float()  # Increase the rate parameter to 5
+
 # Initialize arrays to hold the x, y coordinates and 'p' (some other parameter, possibly neuron activity or position)
 x = np.random.randint(0, width, lenstim)
 y = np.random.randint(0, height, lenstim)
