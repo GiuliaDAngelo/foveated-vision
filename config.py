0947f15d5bf878b836713c378aa7dd62cf387519
Giulia D'Angelo <giulia.dangelo.1990@gmail.com>
2024-10-14 18:38:50 +0200

    adding helpers and paramters

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 
 
-
+# parameters RFs
 width = 2
 height = 2
 num_neurons = width * height
 tau_mem = 1
+
+#parameters events
+lenstim = 100
--- helpers.py
+++ helpers.py
@@ -5,11 +5,39 @@ import sinabs.layers as sl
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
-from helpers import *
+from config import *
 
 
 class RFs:
     def __init__(self, tau_mem):
         self.neuron = sl.LIF(tau_mem=tau_mem)
         self.vmem = []
+        self.spikes = []
 
+def plot_spikes(neurons):
+    plt.figure()
+    neuron_index = 0
+    for x in range(width):
+        for y in range(height):
+            neuron = neurons[y][x]
+            spikes = neuron.spikes
+            for i in range(len(spikes)):
+                plt.plot(range(len(spikes[i].flatten())), spikes[i].flatten(), '|', color='black',
+                         label=f'Neuron {neuron_index}')
+            neuron_index += 1
+    plt.title("Spike raster")
+    plt.xlabel("$t$ [ms]")
+    plt.ylabel("neuron index")
+    # y_axis with neurons index
+    plt.yticks(range(num_neurons), range(num_neurons))
+    plt.show()
+
+
+
+def plot_mempotential(neurons, x, y):
+    plt.figure()
+    plt.plot(range(lenstim - 1), neurons[y][x].vmem)
+    plt.title("LIF membrame dynamics")
+    plt.xlabel("$t$ [ms]")
+    plt.ylabel("$V_{mem}$");
+    plt.show()
