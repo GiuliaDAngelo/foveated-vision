eb24b23179f520e84263be8f32f491ddea0cc9bb
Giulia D'Angelo <giulia.dangelo.1990@gmail.com>
2024-10-16 11:17:47 +0200

    adding credits

--- create_eccentric_retina.py
+++ create_eccentric_retina.py
@@ -1,3 +1,13 @@
+'''
+Giulia D'Angelo, giulia.dangelo@fel.cvut.cz
+
+This script simulate the eccentric representation of the retina using log-polar mapping.
+The code does take isnpiration from the following article:
+
+Chessa, M., Maiello, G., Bex, P. J., & Solari, F. (2016). A space-variant model for motion interpretation across the visual field. Journal of vision, 16(2), 12-12.
+'''
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
